379efeb3c064c795d62fe4d4966520beae3177ec
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2021-08-18 11:17:04 +0530

    introduced middleware - cors  and Added HTML Template

--- app.js
+++ app.js
@@ -3,21 +3,35 @@ const http = require("http");
 const port = 8081;
 const express = require('express');
 const app = express();
-// const app = require('express')();
-// const app= express();
+var cors = require('cors')
+app.use(cors())
+    // const app = require('express')();
+    // const app= express();
+
+const beforeLoad = (req, res, next) => {
+    console.log('before Load');
+    // next();
+    const notValidCondition = 0;
+    if (notValidCondition) {
+        return res.status(401).send();
+    }
+    next();
+    // res.send('form before load');
+}
+app.use(beforeLoad);
 
 app.get('/', function(req, res) {
-    res.send('Hello World')
+    res.send('Hello World');
 });
-// app.post('/', function(req, res) {
-//     console.log(req);
-//     // res.send('post method');
-//     res.send({ message: 'post method' });
-// })
-app.post('/', (req, res) => {
-    console.log(req);
-    // res.send('post method');
-    res.send({ message: 'post method' });
+app.get('/contact', beforeLoad, function(req, res) {
+    // return res.send('contact page');
+    return res.send('<h1>Contact page</h1><p>this is contact page</p>')
+});
+
+
+app.get('**', (req, res) => {
+    res.status(404);
+    res.send({ message: 'Not Found' });
 })
 
 app.listen(port, () => {
